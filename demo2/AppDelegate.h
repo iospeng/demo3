ff1fb9bdbcc8c7c24233c3b161389def4ca04378
iospeng <iospeng@163.com>
2016-04-12 21:51:17 +0800

    Initial Commit

---
+++ demo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo2
+//
+//  Created by 宋伦超 on 16/4/12.
+//  Copyright © 2016年 Mr.Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
